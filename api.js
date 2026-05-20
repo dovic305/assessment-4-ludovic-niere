e4894e366cf8f347d2b65d6466892ae52c30998a
dovic305 <ludovic.niere@univ.edu.vu>
2026-05-20 15:41:30 +1100

    Add new API endpoint for '/api'

--- api.js
+++ api.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('hi ludo: well done');
 });
 
+app.get('/api', (req, res) => {
+  res.send('Boster');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
