497cae44691df693895896dc5a7c1343a240b63b
dovic305 <ludovic.niere@univ.edu.vu>
2026-05-20 15:52:08 +1100

    new api

--- api.js
+++ api.js
@@ -11,6 +11,10 @@ app.get('/api', (req, res) => {
   res.send('Boster');
 });
 
+app.get('/api', (req, res) => {
+  res.send('new api');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
